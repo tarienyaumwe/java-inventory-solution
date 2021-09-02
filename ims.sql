2689692e959235cf650c201e79c522ee28c78aa8
tarienyaumwe <tarie.nyaumwe@gmail.com>
2021-09-02 15:01:44 +0200

    Initial commit

---
+++ ims.sql
@@ -0,0 +1,281 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 02, 2021 at 02:12 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ims`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `currentstocks`
+--
+
+CREATE TABLE `currentstocks` (
+  `productcode` varchar(100) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `currentstocks`
+--
+
+INSERT INTO `currentstocks` (`productcode`, `quantity`) VALUES
+('p2', 30),
+('p1', 1),
+('p10', 0),
+('prod1', 10),
+('prod2', -10),
+('prod4', 80),
+('prod6', 5000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `cid` int(11) NOT NULL,
+  `customercode` varchar(100) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`cid`, `customercode`, `fullname`, `location`, `phone`) VALUES
+(2, 'cus3', 'Kelvin Sithole', 'Gweru', '773080641'),
+(5, 'cus4', 'Farai Masunda', 'Masvingo', '773208997'),
+(6, 'cus5', 'Brian Nyagumbira', 'Gutu', '7731916938'),
+(7, 'cus6', 'Cephas Ziteya', 'Harare', '773151935'),
+(8, 'cus7', 'Marcos', 'Harare', '773135410'),
+(9, 'cus8', 'Raban', 'Chikwanha', '7731919158'),
+(10, 'cus9', 'Tariro Nyaumwe', 'Rusape', '7732011095');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `pid` int(11) NOT NULL,
+  `productcode` varchar(100) NOT NULL,
+  `productname` varchar(50) NOT NULL,
+  `costprice` double NOT NULL,
+  `sellingprice` double NOT NULL,
+  `brand` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`pid`, `productcode`, `productname`, `costprice`, `sellingprice`, `brand`) VALUES
+(74, 'prod4', 'A4 Bond papers', 200, 200, 'Typek'),
+(79, 'prod6', 'Exercise Books A4 (72 page)', 100, 100, 'Schoolar');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchaseinfo`
+--
+
+CREATE TABLE `purchaseinfo` (
+  `purchaseid` int(11) NOT NULL,
+  `suppliercode` varchar(200) NOT NULL,
+  `productcode` varchar(200) NOT NULL,
+  `date` varchar(200) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `totalcost` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `purchaseinfo`
+--
+
+INSERT INTO `purchaseinfo` (`purchaseid`, `suppliercode`, `productcode`, `date`, `quantity`, `totalcost`) VALUES
+(19, 's1', 'p2', 'Wed Jan 14 00:15:19 NPT 2015', 40, 1320),
+(20, 's1', 'p1', 'Wed Jan 07 16:42:44 NPT 2015', 4, 80000),
+(21, 's6', 'p10', 'Tue Jan 06 16:51:44 NPT 2015', 20, 400000),
+(22, 'sup4', 'prod1', 'Thu Jan 15 15:25:45 NPT 2015', 4, 1600),
+(23, 'sup5', 'prod1', 'Thu Jan 15 00:00:00 NPT 2015', 6, 2400),
+(29, 'sup4', 'prod2', 'Fri Jan 16 23:09:17 NPT 2015', 5, 150),
+(30, 'sup5', 'prod1', 'Wed Sep 01 09:59:07 CAT 2021', 10, 10),
+(32, 'sup5', 'prod6', 'Mon Aug 02 13:36:26 CAT 2021', 5000, 500000),
+(33, 'sup5', 'prod4', 'Wed Aug 18 13:36:26 CAT 2021', 80, 16000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salesreport`
+--
+
+CREATE TABLE `salesreport` (
+  `salesid` int(11) NOT NULL,
+  `date` varchar(40) NOT NULL,
+  `productcode` varchar(100) NOT NULL,
+  `customercode` varchar(100) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `revenue` double NOT NULL,
+  `soldby` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `salesreport`
+--
+
+INSERT INTO `salesreport` (`salesid`, `date`, `productcode`, `customercode`, `quantity`, `revenue`, `soldby`) VALUES
+(6, 'Thu Sep 02 10:08:27 CAT 2021', 'prod4', 'cus3', 1, 450, 'user4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `sid` int(11) NOT NULL,
+  `suppliercode` varchar(100) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `phone` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`sid`, `suppliercode`, `fullname`, `location`, `phone`) VALUES
+(69, 'sup5', 'manish', 'Bindura', '718078644'),
+(68, 'sup4', 'sia', 'Chimanimani', '773865789'),
+(143, 'sup6', 'TreeHouse', 'Cnr Samora and Chinhoi, Harare', '772458798');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `category` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullname`, `location`, `phone`, `username`, `password`, `category`) VALUES
+(54, 'Tariro Nyaumwe', 'Harare', '9849284991', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'ADMINISTRATOR'),
+(59, 'Kelvin Sithole', 'chirundu', '1548761', 'user', 'affec3b64cf90492377a8114c86fc093', 'ADMINISTRATOR');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `purchaseinfo`
+--
+ALTER TABLE `purchaseinfo`
+  ADD PRIMARY KEY (`purchaseid`);
+
+--
+-- Indexes for table `salesreport`
+--
+ALTER TABLE `salesreport`
+  ADD PRIMARY KEY (`salesid`);
+
+--
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+
+--
+-- AUTO_INCREMENT for table `purchaseinfo`
+--
+ALTER TABLE `purchaseinfo`
+  MODIFY `purchaseid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `salesreport`
+--
+ALTER TABLE `salesreport`
+  MODIFY `salesid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=144;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
